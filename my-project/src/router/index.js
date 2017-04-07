92a329d295ba98ca4458f7ca8798d26f0e836415
tenghui <1246623393@qq.com>
2017-04-07 23:55:15 +0800

    ~route

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Hello2 from '@/components/Hello2'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/test',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/test2',
+      name: 'Hello world2',
+      component: Hello2
     }
   ]
 })
